a701b2a3145d2e1d0423269abfa2be9cb9e7a3fd
Pedro <pgdtsouza@gmail.com>
2025-11-06 15:31:32 -0300

    Add sidebar items toggle logic

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,4 @@
+/* TOGGLE MENU */
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 const openButton = document.querySelector('.hamburguer-menu-icon-wrapper');
@@ -21,7 +22,7 @@ function closeSidebar()
 }
 
 
-/* CLICK OUTSIDE */
+/* CLICK OUTSIDE TO CLOSE */
 document.addEventListener('click', (event) =>
 {
     const clickInside = sidebar.contains(event.target) || openButton.contains(event.target);
@@ -31,3 +32,99 @@ document.addEventListener('click', (event) =>
         closeSidebar();
     }
 })
+
+
+/* SHOW DESCRIPTION */
+const howToPlayButton = document.querySelector('.sidebar-how-to-play-button');
+const newGameButton = document.querySelector('.sidebar-new-game-button');
+const aboutButton = document.querySelector('.sidebar-about-button');
+
+
+howToPlayButton.addEventListener('click', (event) => {toggleDescription(event.target)});
+newGameButton.addEventListener('click', (event) => {toggleDescription(event.target)});
+aboutButton.addEventListener('click', (event) => {toggleDescription(event.target)});
+
+function getDescription(button)
+{
+    const description = document.createElement('div');
+    description.classList.add('sidebar-item-description');
+
+    if (button === 'howToPlay')
+    {
+        const text = document.createElement('p');
+
+        text.textContent = `De 2 a 5 times.
+        Cada time com 2 a 3 pessoas.
+        
+        Principais regras:
+        Round 1 = livre
+        Round 2 = 1 palavra
+        Round 3 = mímica`;
+
+        description.appendChild(text);
+    }
+    else if (button === 'newGame')
+    {
+        const text = document.createElement('p');
+
+        text.textContent = `De 2 a 5 times.
+        Cada time com 2 a 3 pessoas.
+        
+        Principais regras:
+        Round 1 = livre
+        Round 2 = 1 palavra
+        Round 3 = mímica`  
+
+        description.appendChild(text);
+    }
+    else if (button === 'about')
+    {
+        const text = document.createElement('p');
+
+        text.textContent = `De 2 a 5 times.
+        Cada time com 2 a 3 pessoas.
+        
+        Principais regras:
+        Round 1 = livre
+        Round 2 = 1 palavra
+        Round 3 = mímica`
+
+        description.appendChild(text);
+    }
+
+    return description;
+}
+
+function toggleDescription(target)
+{
+    let parameter = '';
+
+    if (target.classList.contains('sidebar-how-to-play-button'))
+    {
+        parameter = 'howToPlay';
+    }
+    else if (target.classList.contains('sidebar-new-game-button'))
+    {
+        parameter = 'newGame';
+    }
+    else if (target.classList.contains('sidebar-about-button'))
+    {
+        parameter = 'about';
+    }
+
+    console.log(parameter);
+
+
+    let targetDescription = target.querySelector('.sidebar-item-description');
+
+    if (targetDescription)
+    {
+        console.log('on');
+        targetDescription.remove();
+    }
+    else
+    {
+        console.log('off');
+        target.appendChild(getDescription(parameter));
+    }
+}
